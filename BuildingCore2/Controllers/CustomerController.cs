7525b00f742e37d3c0960461a4389eb72a9b3811
itdev-lenovo <it1devops@gmail.com>
2020-03-31 16:57:25 +0530

    files added

---
+++ BuildingCore2/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingCore2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+       // 1.Get a Single Customer from  the repository by an api call.
+    //2.Use PostMan to consume  the Get API Call.
+//  3.Include another api call to Add Customer to the database. Use POST Api call and use PostMan  // to insert the data.
+//  4.Include another api call to Update Customer in the database. Use PUT API Call and Return   Correct Status
+
+    }
+}
